8ab7bf7e521b3ccea4f457486e911aa7daf3a8be
tony555t <tonytullah764@gmail.com>
2024-01-18 10:25:25 +0300

    links

---
+++ get.php
@@ -0,0 +1,20 @@
+<?php
+print_r($_GET);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$id = 10;
+?>
+
+<a href="index.php?id=200">click here</a>
+
+</body>
+</html>
