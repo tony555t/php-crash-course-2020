1778bdc651fbdfded74d867527d8a671d96b4a35
tony555t <tonytullah764@gmail.com>
2024-01-19 12:39:13 +0300

    cookies

---
+++ cookie.php
@@ -0,0 +1,15 @@
+<?php
+$name="SomeName";
+$value=100;
+$expiration=time ()+(60*60*24*7);
+
+setcookie('name','value','expiration');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Cookie Example</title>
+
+</head>
+<body></body>
