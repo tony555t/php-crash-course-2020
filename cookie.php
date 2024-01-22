e28fadcee4f7fe557363f1a791a69be14bb68c44
tony555t <tonytullah764@gmail.com>
2024-01-22 11:31:52 +0300

    cookie message

--- cookie.php
+++ cookie.php
@@ -1,31 +1,28 @@
 <?php
-$name="SomeName";
-$value=100;
-$expiration=time ()+(60*60*24*7);
+$name = "SomeName";
+$value = 'This website uses cookies to ensure you get the best experience. <a href="#">Learn more</a>';
+$expiration = time() + (60 * 60 * 24 * 7);
+// Concatenate $name and $value with a separator
+//$cookieName = $name . '_' . $value;
 
-setcookie('name','value','expiration');
+setcookie($name,$value, $expiration);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>Cookie Example</title>
-
+<head>
+    <meta charset="utf-8" />
+    <title>Document</title>
+    <!-- Add the favicon link here -->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
     <?php
-
-if(isset($_COOKIE["SomeName"])){
-    $someOne=$_COOKIE["SomeName"];
-    echo $someOne;
-}else{
-    $someOne="";
-}
-?>
-
-
-
-
-
+    if (isset($_COOKIE["SomeName"])) {
+        $someOne = $_COOKIE["SomeName"];
+        echo $someOne;
+    } else {
+        $someOne = "";
+    }
+    ?>
 </body>
 </html>
