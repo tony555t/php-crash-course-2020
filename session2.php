583757698aacb70a7cdf6a21e18547ed6b7a85bc
tony555t <tonytullah764@gmail.com>
2024-01-22 16:10:50 +0300

    sesion  message

---
+++ session2.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+echo $_SESSION['greetings'];
+?>
+
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Welcome to my website</title>
+</head>
+<body>
+</body>
+</html>
