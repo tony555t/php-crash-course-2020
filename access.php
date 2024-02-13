b343b7cdcf98ca9d3faa6b90d3594176a64ff1c8
tony555t <tonytullah764@gmail.com>
2024-02-13 15:11:42 +0300

    getter&setter

--- access.php
+++ access.php
@@ -14,4 +14,33 @@ class Towns{
         $this->finance=$finance;
         $this->population=$population;
     }
+     //getter for the mayor property<
+
+     public function getMayor(){
+        return $this->mayor;
+
+    }
+ // setter for the mayor property
+ public  function setMayor($newMayor){
+    $this->mayor=$newMayor;
+
+ }
+ //getter for the finance property
+ public function getFinance(){
+    return $this->finance;
+ }
+ //setter for the Finance property
+ public function setFinance($newFinance){
+    $this->finance=$newFinance;
+ }
+ // getter for the popularity property
+ public function getPopulation(){
+
+    return $this->Population;
+ }
+// setter for the popularity property
+ public function setPopulation($newpopulation){
+  $this->population=$newpopulation;
+}
+
 }
